c00de197a1c44254a63fe1cc26ab7d899a8856a2
Leandro Albrieu <leandro1237@hotmail.com.ar>
2024-01-08 08:50:13 -0300

    primer commit

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,10 +3,31 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
+  sequelize.define('Pokemon', {
+    id:{
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    nombre: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    imagen:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    vida:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    ataque:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    defensa:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   });
 };
