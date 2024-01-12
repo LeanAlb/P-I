97fc274d7032df367386db6c7c149c91f6e07cc9
Leandro Albrieu <leandro1237@hotmail.com.ar>
2024-01-12 09:48:28 -0300

    Home renderizada

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -2,32 +2,13 @@ import React from "react";
 import SearchBar from "../Searchbar/Searchbar";
 import style from './HomePage.module.css'
 import Cards from "../Cards/Cards";
-import { useSelector, useDispatch} from "react-redux";
-import { useEffect } from "react";
-import { showPokemons } from "../../redux/actions";
 
 
 
 const HomePage=()=>{
-    // const dispatch=useDispatch()
-    // const allPokemons=useSelector((state)=>state.allPokemons)
-
-    // // useEffect(()=>{
-    // //     if(!allPokemons.length){
-    // //         dispatch(showPokemons())
-    // //     }
-    // // },[])
-    // if(!allPokemons.length){
-    //     return (
-    //         <div>
-    //         Cargando...
-    //     </div>
-    //     )
-    // }
     return(
         <div className={style.home}>
             <SearchBar/>
-            {/* <Cards pokemons={allPokemons}/> */}
         </div>
     )
 }
